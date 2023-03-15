714478a1c32f132c15d822b799849a31f09b3315
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-15 08:16:43 -0300

    Finalizando a criaçao da barra de navegação do sistema

--- src/components/Navba.jsx
+++ src/components/Navba.jsx
@@ -1,19 +1,39 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-
+import { useRef } from 'react'
+import './Navbar.css'
 const Navba = () => {
-    return (
-        <header>
-            <h1>
-                Login <i className="fas fa-sign-in-alt"></i>
-            </h1>
-            <nav>
-                <a href="#"> <i className="fas fa-home"></i> Home</a>
-                <a href="#"> <i className="fas fa-user"></i> About</a>
-                <a href="#"> <i className="fas fa-envelope"></i> Contact</a>
-                <a href="#"> <i className="fas fa-question"></i> Help</a>
-            </nav>
-        </header>
-    )
+    const navRef = useRef();
+
+	const showNavbar = () => {
+		navRef.current.classList.toggle(
+			"responsive_nav"
+		);
+	};
+
+	return (
+		<header>
+			<h3>
+                <a href="https://github.com/benetesla">My Portfolio</a>
+            </h3>
+			<nav ref={navRef}>
+				<a href="/#"><i className="fas fa-home"></i> Home</a>
+				<a href="/#"> <i className="fas fa-user"></i> About me</a>
+				<a href="/#"> <i className="fas fa-envelope"></i> Contact</a>
+				<a href="/#"> <i className="fas fa-briefcase"></i> Portfolio</a>
+				<button
+					className="nav-btn nav-close-btn"
+					onClick={showNavbar}>
+					<i className="fas fa-times"></i>
+				</button>
+			</nav>
+			<button
+				className="nav-btn"
+				onClick={showNavbar}>
+				<i className="fas fa-bars"></i>
+			</button>
+		</header>
+	);
 }
 
 export default Navba
